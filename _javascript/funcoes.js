67735b1f3d4b4fe03424fb903726492d136a9d37
DaviPissinati <davi.pissinati@gmail.com>
2021-05-26 01:21:40 -0300

    first commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+/*function mudaFoto(foto) {
+    document.getElementById("icone").src= foto;
+    } */
+   function mudafoto(foto) {
+    document.getElementById("icone").src = "_imagens/" + foto + ".png";
+}   
+
+    /* <script type="text/javascript" src="_javascript/funcoes.js"></script>  <script src="_javascript/funcoes.js"></script> */
